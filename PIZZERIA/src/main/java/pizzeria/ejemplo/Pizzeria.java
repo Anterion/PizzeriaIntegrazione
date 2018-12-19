c8a5525c32abfee844f7955cc8227ae6c94e7cd2
Sergio Vacas <vaks@hotmail.es>
2018-12-19 23:39:25 +0100

    Hecho hasta la definición del despliegue (transparencia 18)

---
+++ PIZZERIA/src/main/java/pizzeria/ejemplo/Pizzeria.java
@@ -0,0 +1,9 @@
+package pizzeria.ejemplo;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Pizzeria Funcionando")
+public class Pizzeria extends ServletProcessApplication{
+
+}

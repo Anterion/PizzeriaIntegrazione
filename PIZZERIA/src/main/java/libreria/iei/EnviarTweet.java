2793fe69e0cc226c18f62f7dededd86348d35beb
Sergio Vacas <vaks@hotmail.es>
2018-12-20 20:01:56 +0100

    acabado hasta transparencia 55

--- PIZZERIA/src/main/java/libreria/iei/EnviarTweet.java
+++ PIZZERIA/src/main/java/libreria/iei/EnviarTweet.java
@@ -16,10 +16,10 @@ public class EnviarTweet implements JavaDelegate {
 		String pizza = (String) execution.getVariable("IDNombrePizza");
 		double precio = (Double) execution.getVariable("IDPrecio");
 		String entrega = (String) execution.getVariable("IDEntrega");
-		String consumerKey = "mq0uRzZOFlkXaPVwzm7Z9ANjM";
-		String consumerSecret = "MT2jo4qg8KZFAfpEP0GMs8Ms1lJykE61QY5Z6ttSi976WgIXoU";
-		String accessToken = "1073167446452899840-PuIhJQdcpdUQYio6A8Rc9CmCaSo15r";
-		String accessTokenSecret = "RL0Q8OXr2hnzeNuG2qLdLhH9sG8susVmV1vF0CWy0gSrJ";
+		String consumerKey = "";
+		String consumerSecret = "";
+		String accessToken = "";
+		String accessTokenSecret = "";
 		ConfigurationBuilder cb = new ConfigurationBuilder();
 		cb.setDebugEnabled(true).setOAuthConsumerKey(consumerKey)
 		.setOAuthConsumerSecret(consumerSecret).setOAuthAccessToken(accessToken)
